c266dbc2e29d9f81798c410f7303330e4d28fc78
Bernát Kalló <kallo.bernat@gmail.com>
2025-06-04 14:25:01 +0200

    vercel deploy

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const app = express();
+app.get("/api/getemoji", async (req, res) => {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4.1-nano",
+    temperature: 0,
+    messages: [
+      { role: "developer", content: "Generate an emoji from the text: " },
+      { role: "user", content: req.query.text },
+    ],
+  });
+  res.send(response.choices[0].message.content);
+});
+app.use(express.static("./public"));
+export default app;
