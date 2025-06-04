e5caac9acb856dd65571617bfbce5a5fb9441e96
Bernát Kalló <kallo.bernat@gmail.com>
2025-06-04 14:25:16 +0200

    ui: loading indicator

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ const text = document.getElementById("text");
 const emoji = document.getElementById("emoji");
 text.addEventListener("keydown", async (event) => {
   if (event.key == "Enter") {
+    emoji.textContent = "…";
     const response = await fetch("/api/getemoji?text=" + text.value);
     emoji.textContent = await response.text();
     console.log("Hello word");
