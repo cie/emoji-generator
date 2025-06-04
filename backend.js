8e8cc94e1f848b474ff787b4b95601ce70a96574
Bernát Kalló <kallo.bernat@gmail.com>
2025-06-04 14:09:09 +0200

    backend call to openai api, design

--- backend.js
+++ backend.js
@@ -1,12 +1,24 @@
 import express from "express";
+import OpenAI from "openai";
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 const app = express();
 
-app.get("/api/getemoji", (req, res) => {
-  res.send("😂");
+app.get("/api/getemoji", async (req, res) => {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4.1-nano",
+    temperature: 0,
+    messages: [
+      { role: "developer", content: "Generate an emoji from the text: " },
+      { role: "user", content: req.query.text },
+    ],
+  });
+  res.send(response.choices[0].message.content);
 });
 app.use(express.static("./frontend"));
 
 app.listen(3000, () => {
-  console.log("a backend fut");
+  console.log(`A backend fut`);
 });
